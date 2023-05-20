931b994198dd7215c88d8a5d121458480ce2341c
wondonggyun <waterflame1@naver.com>
2023-05-20 23:56:53 +0900

    observer 예제 업데이트

---
+++ observer/1-1-example.ts
@@ -0,0 +1,14 @@
+{
+  class WeatherData {
+    getTemperature() {
+      console.log("get Temperature!");
+    }
+    getHumidity() {
+      console.log("get Humidity!");
+    }
+    getPressure() {
+      console.log("get Pressure!");
+    }
+    measurementsChanged() {}
+  }
+}
